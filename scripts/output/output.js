15b0399036b8f9d3e5093202bc7c7e7484b44abd
101dal <66965262+101dal@users.noreply.github.com>
2021-04-22 20:21:36 +0200

    2

--- scripts/output/output.js
+++ scripts/output/output.js
@@ -4,6 +4,7 @@ function copy() {
     var copyText = document.querySelector("#output1");
     copyText.select();
     document.execCommand("copy");
+    alert("Element copied")
 }
 
 document.querySelector("#outputbutton").addEventListener("click", copy);
@@ -11,5 +12,8 @@ document.querySelector("#outputbutton").addEventListener("click", copy);
 
 function advancementoutput() {
     var mode = document.getElementById('advancement-mode').value;
-    document.getElementById('output1').value = '/advancement ' + mode;
+    var player = document.getElementById('player').value;
+    var literal = document.getElementById('literal').value;
+    var advancementselect = document.getElementById('advancementselect').value;
+    document.getElementById('output1').value = '/advancement ' + mode + ' ' + player + ' ' + literal + ' ' + advancementselect;
 }
